a800a104228710422447f3034cc1eb43baaf6ef3
smoorthy320 <smoorthy320@gmail.com>
2017-11-14 12:14:47 +0530

    ver 1.1

--- src/com/emp/Solution.java
+++ src/com/emp/Solution.java
@@ -15,18 +15,30 @@ public class Solution {
 	public static void main(String[] args) throws IOException {
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 		Session session = sf.openSession();
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter id:");
-		int id=Integer.valueOf(bf.readLine());
-		System.out.print("Enter name:");
-		String name=bf.readLine();
-		System.out.println("Enter email:");
-		String emailId=bf.readLine();
-		System.out.println("Enter Phone no:");
-		long phone=Long.valueOf(bf.readLine());
 		
-		Employee employee = new Employee(id, name, emailId, phone);
-		System.out.println(employee.getId()+" "+employee.getName()+" "+employee.getEmailId()+" "+employee.getPhone());
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int i = 0;
+		while (i < 3) {
+			i++;
+			session.beginTransaction();
+			System.out.print("Enter name:");
+			String name = bf.readLine();
+			System.out.println("Enter email:");
+			String emailId = bf.readLine();
+			System.out.println("Enter Phone no:");
+			long phone = Long.valueOf(bf.readLine());
+			System.out.println("Basicpay:");
+			double basicPay = Double.valueOf(bf.readLine());
+			System.out.println("HRA:");
+			float houseRentAllowance = Float.valueOf(bf.readLine());
+			float providentFund = Float.valueOf(bf.readLine());
+			Employee employee = new Employee(name, emailId, phone, basicPay, houseRentAllowance, providentFund);
+			System.out.println(employee.getName() + " " + employee.getEmailId() + " " + employee.getPhone()
+					+ employee.getBasicPay() + " " + employee.getHouseRentAllowance() + " "
+					+ employee.getProvidentFund());
+			session.save(employee);
+			session.getTransaction().commit();
+		}
 		session.close();
 		System.exit(0);
 	}
--- src/com/emp/sample/Employee.java
+++ src/com/emp/sample/Employee.java
@@ -1,32 +1,46 @@
 package com.emp.sample;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
+@Table(name = "Emp_details")
 public class Employee {
 	@Id
+	@GeneratedValue
+	@Column(name = "Emp_id")
 	private int id;
+	@Column(name = "Emp_name",length=150,nullable=false)
 	private String name;
+	@Column(name = "Email_id")
 	private String emailId;
+	@Column(name = "Phone")
 	private long phone;
-	
-	public Employee(int id, String name, String emailId, long phone) {
-		super();
-		this.id = id;
+	@Column(name = "BP")
+	private double basicPay;
+	@Column(name = "HRA")
+	private float houseRentAllowance;
+	@Column(name = "PF")
+	private float providentFund;
+	@Transient
+	private float netSalary;
+
+	public Employee( String name, String emailId, long phone, double basicPay, float houseRentAllowance,
+			float providentFund) {
+
 		this.name = name;
 		this.emailId = emailId;
 		this.phone = phone;
-		
+		this.basicPay = basicPay;
+		this.houseRentAllowance = houseRentAllowance;
+		this.providentFund = providentFund;
 	}
 
-	public int getId() {
-		return id;
-	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
@@ -52,6 +66,37 @@ public class Employee {
 		this.phone = phone;
 	}
 
+	public double getBasicPay() {
+		return basicPay;
+	}
 
+	public void setBasicPay(double basicPay) {
+		this.basicPay = basicPay;
+	}
 
+	public float getHouseRentAllowance() {
+		return houseRentAllowance;
+	}
+
+	public void setHouseRentAllowance(float houseRentAllowance) {
+		this.houseRentAllowance = houseRentAllowance;
+	}
+
+	public float getProvidentFund() {
+		return providentFund;
+	}
+
+	public void setProvidentFund(float providentFund) {
+		this.providentFund = providentFund;
+	}
+
+	public float getNetSalary() {
+		return netSalary;
+	}
+
+	public void setNetSalary(float netSalary) {
+		this.netSalary = netSalary;
+	}
+
+	
 }
