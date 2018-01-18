9ced98ea1659d83bb1079dd8f299531c75607f33
Sundaramoorthy S <sundars679@gmail.com>
2018-01-18 12:06:31 +0530

    Angular  ver 2.0 binding

--- Angular/Work/js/app.js
+++ Angular/Work/js/app.js
@@ -1,3 +1,13 @@
 (function(){
     var app = angular.module("Work",[]);
+    app.controller("StoreController",function(){
+        this.product = item
+    });
+    var item = {
+        name : "Shampoo",
+        price : 220.1,
+        description : "Its just a shampoo",
+        canPurchase : false,
+        soldOut : true
+    };
 })();
