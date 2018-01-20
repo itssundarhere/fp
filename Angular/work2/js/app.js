7c8d46c94904b80b401c2b3f40d638b5b6a15501
Sundaramoorthy S <sundars679@gmail.com>
2018-01-20 11:57:48 +0530

    ver 4.0

--- Angular/work2/js/app.js
+++ Angular/work2/js/app.js
@@ -3,15 +3,24 @@
     app.controller("StoreController", function ($http) {
         var pb = this;
         this.contacts = [];
-        this.delete = function(loc) {
-            pb.contacts.splice(loc, 1)
+        this.delete = function (loc, id) {
+            $http.delete("http://172.16.73.122:3000/contacts/" + id).then(function (res) {
+                pb.contacts.splice(loc, 1)
+            }, function (res) {
+                console.log("Error Deleting");
+            });
         };
-        this.toggleEdit = function(c) {
-            c.editable = !c.editable?true:false;
+        this.toggleEdit = function (c) {
+            if (c.editable) {
+                $http.put("http://172.16.73.122:3000/contacts/" + c.id, c).then(function (res) {
+                    console.log("Success Updated")
+                }, function (res) {});
+            }
+            c.editable = !c.editable ? true : false;
         };
-        $http.get("phone-book.json").then(function(res) {
-            pb.contacts = res.data.contacts
-        },function(res) {
+        $http.get("http://172.16.73.122:3000/contacts").then(function (res) {
+            pb.contacts = res.data
+        }, function (res) {
             console.log("Error")
         })
     });
